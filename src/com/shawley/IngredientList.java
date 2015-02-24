17668dbfc27bcd18e48aaa461dcc88bf2c334efa
Laura Turley <lozyness@gmail.com>
2015-02-24 23:35:10 +0000

    Ingredient list can be created and output into the terminal through tests - no interface

---
+++ src/com/shawley/IngredientList.java
@@ -0,0 +1,45 @@
+package shawley;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IngredientList {
+
+    private List<String> ingredientList = new ArrayList<String>();
+
+    public boolean isEmpty() {
+        return this.ingredientList.size() == 0;
+    }
+
+    public int size() {
+        return this.ingredientList.size();
+    }
+
+    public void addIngredient(String ingredient) {
+        this.ingredientList.add(ingredient);
+    }
+
+    private Iterator<String> iterator() {
+        return this.ingredientList.iterator();
+    }
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        Iterator<String> iter = this.iterator();
+        while(iter.hasNext()) {
+            String ingredient = iter.next();
+            result.append(ingredient);
+            result.append("\n");
+        }
+        result.deleteCharAt(result.length()-1);
+        return result.toString();
+    }
+
+    public String getShoppingList() {
+        return this.toString();
+    }
+}
---
+++ src/test/IngredientListTest.java
@@ -0,0 +1,71 @@
+import org.junit.Before;
+import org.junit.Test;
+import shawley.IngredientList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class IngredientListTest {
+
+    private IngredientList ingredientsList;
+
+    @Before
+    public void setup() {
+        this.ingredientsList = new IngredientList();
+    }
+
+    @Test
+    public void testListIsEmpty() {
+        assertTrue(this.ingredientsList.isEmpty());
+    }
+
+    @Test
+    public void testSizeIsZeroIfListIsEmpty(){
+        assertEquals(0, this.ingredientsList.size());
+    }
+
+    @Test
+    public void testAddingItemToListIncrementsSize() {
+        this.ingredientsList.addIngredient("Ingredient");
+        assertEquals(1, this.ingredientsList.size());
+    }
+
+    @Test
+    public void testAddingFiveItemsToListIncrementsSizeByFive() {
+        String baseName = "Ingredient";
+        List<String> ingredients = new ArrayList<String>();
+        for (int i = 0; i<5; i++) {
+            ingredients.add(baseName+i);
+        }
+        Iterator<String> iter = ingredients.iterator();
+        while(iter.hasNext()) {
+            String ingredient = iter.next();
+            this.ingredientsList.addIngredient(ingredient);
+        }
+        assertEquals(ingredients.size(), this.ingredientsList.size());
+    }
+
+    @Test
+    public void testOutputIngredientsListWithOneItemMatchesExpectation() {
+        String list = "Ingredient";
+        this.ingredientsList.addIngredient(list);
+        assertEquals(list, this.ingredientsList.toString());
+    }
+
+    @Test
+    public void testOutputIngredientsListWithThreeItemsMatchesExpectation() {
+        String list = "Ingredient0" +
+                "\nIngredient1" +
+                "\nIngredient2";
+        String baseName = "Ingredient";
+        this.ingredientsList.addIngredient(baseName+"0");
+        this.ingredientsList.addIngredient(baseName+"1");
+        this.ingredientsList.addIngredient(baseName+"2");
+        assertEquals(list, this.ingredientsList.getShoppingList());
+    }
+
+}
