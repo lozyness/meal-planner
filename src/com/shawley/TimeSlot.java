9aa4265be80fc58606185b4458740483c21a4f5d
Laura Turley <lozyness@gmail.com>
2015-02-25 19:22:32 +0000

    Added basic timeslot model

--- src/com/shawley/TimeSlot.java
+++ src/com/shawley/TimeSlot.java
@@ -4,4 +4,51 @@ package shawley;
  * Created by laura on 25/02/15.
  */
 public class TimeSlot {
+
+    private static final int MORNING = 0;
+    private static final int AFTERNOON = 1;
+    private static final int EVENING = 2;
+    private static final int MISC = 3;
+
+    private int timeslot;
+
+    public TimeSlot(int timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    private static TimeSlot createTimeSlot(int timeslot) {
+        return new TimeSlot(timeslot);
+    }
+
+    public static TimeSlot createMorningTimeSlot() {
+        return TimeSlot.createTimeSlot(TimeSlot.MORNING);
+    }
+
+    public static TimeSlot createAfternoonTimeSlot() {
+        return TimeSlot.createTimeSlot(TimeSlot.AFTERNOON);
+    }
+
+    public boolean isMorning() {
+        return this.timeslot == TimeSlot.MORNING;
+    }
+
+    public boolean isAfternoon() {
+        return this.timeslot == TimeSlot.AFTERNOON;
+    }
+
+    public boolean isEvening() {
+        return this.timeslot == TimeSlot.EVENING;
+    }
+
+    public boolean isMisc() {
+        return this.timeslot == TimeSlot.MISC;
+    }
+
+    public static TimeSlot createEveningTimeSlot() {
+        return TimeSlot.createTimeSlot(TimeSlot.EVENING);
+    }
+
+    public static TimeSlot createMiscTimeSlot() {
+        return TimeSlot.createTimeSlot(TimeSlot.MISC);
+    }
 }
--- src/tests/test/TimeSlotTest.java
+++ src/tests/test/TimeSlotTest.java
@@ -7,11 +7,46 @@ import static org.junit.Assert.*;
 
 public class TimeSlotTest {
 
+    private TimeSlot timeslot;
+
     @Test
     public void canCreateMorningTimeslot() {
-        TimeSlot timeslot = null;
-        assertNotNull("Timeslot should have been instantiated", timeslot);
-        assertTrue("Timeslot created is of wrong type", timeslot.isMorning());
+        this.timeslot = TimeSlot.createMorningTimeSlot();
+        assertNotNull("Timeslot should have been instantiated", this.timeslot);
+        assertTrue("Timeslot created is of wrong type", this.timeslot.isMorning());
+        assertFalse("Timeslot created is of wrong type (afternoon)", this.timeslot.isAfternoon());
+        assertFalse("Timeslot created is of wrong type (evening)", this.timeslot.isEvening());
+        assertFalse("Timeslot created is of wrong type (misc)", this.timeslot.isMisc());
+    }
+
+    @Test
+    public void canCreateAfternoonTimeslot() {
+        this.timeslot = TimeSlot.createAfternoonTimeSlot();
+        assertNotNull("Timeslot should have been instantiated", this.timeslot);
+        assertTrue("Timeslot created is of wrong type", this.timeslot.isAfternoon());
+        assertFalse("Timeslot created is of wrong type", this.timeslot.isMorning());
+        assertFalse("Timeslot created is of wrong type (evening)", this.timeslot.isEvening());
+        assertFalse("Timeslot created is of wrong type (misc)", this.timeslot.isMisc());
+    }
+
+    @Test
+    public void canCreateEveningTimeslot() {
+        this.timeslot = TimeSlot.createEveningTimeSlot();
+        assertNotNull("Timeslot should have been instantiated", this.timeslot);
+        assertTrue("Timeslot created is of wrong type (evening)", this.timeslot.isEvening());
+        assertFalse("Timeslot created is of wrong type", this.timeslot.isMorning());
+        assertFalse("Timeslot created is of wrong type", this.timeslot.isAfternoon());
+        assertFalse("Timeslot created is of wrong type (misc)", this.timeslot.isMisc());
+    }
+
+    @Test
+    public void canCreateMiscTimeslot() {
+        this.timeslot = TimeSlot.createMiscTimeSlot();
+        assertNotNull("Timeslot should have been instantiated", this.timeslot);
+        assertTrue("Timeslot created is of wrong type (misc)", this.timeslot.isMisc());
+        assertFalse("Timeslot created is of wrong type (evening)", this.timeslot.isEvening());
+        assertFalse("Timeslot created is of wrong type", this.timeslot.isMorning());
+        assertFalse("Timeslot created is of wrong type", this.timeslot.isAfternoon());
     }
 
 }
